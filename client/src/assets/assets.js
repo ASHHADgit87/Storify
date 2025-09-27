a77c2596b9e953761152580229fa60d3d473221a
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2025-09-27 17:05:27 +0500

    added more assets adn refined assets.js file

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,14 +1,87 @@
-// assets.js
+import backpack1 from "./backpack1.jpg";
+import backpack2 from "./backpack2.jpg";
+import backpack3 from "./backpack3.jpg";
+import backpack4 from "./backpack4.jpg";
 
-import backpack from "./backpack.jpg";
-import chair from "./chair.jpg";
-import console from "./console.jpg";
-import gamingchair from "./gamingchair.jpg";
-import gamingmouse from "./gamingmouse.jpg";
-import handbag from "./handbag.jpg";
-import headphones from "./headphones.jpg";
-import app from "./app.jpg";
-import play from "./play.jpg";
+
+import chair1 from "./chair1.jpg";
+import chair2 from "./chair2.jpg";
+import chair3 from "./chair3.jpg";
+import chair4 from "./chair4.jpg";
+
+import console1 from "./console1.jpg";
+import console2 from "./console2.jpg";
+import console3 from "./console3.jpg";
+import console4 from "./console4.jpg";
+
+import gamingchair1 from "./gamingchair1.jpg";
+import gamingchair2 from "./gamingchair2.jpg";
+import gamingchair3 from "./gamingchair3.jpg";
+import gamingchair4 from "./gamingchair4.jpg";
+
+
+import gamingmouse1 from "./gamingmouse1.jpg";
+import gamingmouse2 from "./gamingmouse2.jpg";
+import gamingmouse3 from "./gamingmouse3.jpg";
+import gamingmouse4 from "./gamingmouse4.jpg";
+
+
+import handbag1 from "./handbag1.jpg";
+import handbag2 from "./handbag2.jpg";
+import handbag3 from "./handbag3.jpg";
+import handbag4 from "./handbag4.jpg";
+
+
+import headphones1 from "./headphones1.jpg";
+import headphones2 from "./headphones2.jpg";
+import headphones3 from "./headphones3.jpg";
+import headphones4 from "./headphones4.jpg";
+
+
+import hoodie1 from "./hoodie1.jpg";
+import hoodie2 from "./hoodie2.jpg";
+import hoodie3 from "./hoodie3.jpg";
+import hoodie4 from "./hoodie4.jpg";
+
+import lamp1 from "./lamp1.jpg";
+import lamp2 from "./lamp2.jpg";
+import lamp3 from "./lamp3.jpg";
+import lamp4 from "./lamp4.jpg";
+
+import laptop1 from "./laptop1.jpg";
+import laptop2 from "./laptop2.jpg";
+import laptop3 from "./laptop3.jpg";
+import laptop4 from "./laptop4.jpg";
+
+import mechanicalkeyboard1 from "./mechanicalkeyboard1.jpg";
+import mechanicalkeyboard2 from "./mechanicalkeyboard2.jpg";
+import mechanicalkeyboard3 from "./mechanicalkeyboard3.jpg";
+import mechanicalkeyboard4 from "./mechanicalkeyboard4.jpg";
+
+import mobile1 from "./mobile1.jpg";
+import mobile2 from "./mobile2.jpg";
+import mobile3 from "./mobile3.jpg";
+import mobile4 from "./mobile4.jpg";
+import shoes1 from "./shoes1.jpg";
+import shoes2 from "./shoes2.jpg";
+import shoes3 from "./shoes3.jpg";
+import shoes4 from "./shoes4.jpg";
+
+import smartwatch1 from "./smartwatch1.jpg";
+import smartwatch2 from "./smartwatch2.jpg";
+import smartwatch3 from "./smartwatch3.jpg";
+import smartwatch4 from "./smartwatch4.jpg";
+
+import table1 from "./table1.jpg";
+import table2 from "./table2.jpg";
+import table3 from "./table3.jpg";
+import table4 from "./table4.jpg";
+
+import tshirt1 from "./tshirt1.jpg";
+import tshirt2 from "./tshirt2.jpg";
+import tshirt3 from "./tshirt3.jpg";
+import tshirt4 from "./tshirt4.jpg";
+
 import heroimage1 from "./heroimage1.jpg";
 import heroimage2 from "./heroimage2.jpg";
 import heroimage3 from "./heroimage3.jpg";
@@ -16,41 +89,82 @@ import heroimage4 from "./heroimage4.jpg";
 import heroimage5 from "./heroimage5.jpg";
 import heroimage6 from "./heroimage6.jpg";
 
-import hoodie from "./hoodie.jpg";
-import lamp from "./lamp.jpg";
-import laptop from "./laptop.jpg";
+import app from "./app.jpg";
+import play from "./play.jpg";
 import logo from "./logo.svg";
-import mechanicalkeyboard from "./mechanicalkeyboard.jpg";
-import mobile from "./mobile.jpg";
-import smartwatch from "./smartwatch.jpg";
-import sneakers from "./sneakers.jpg";
-import table from "./table.jpg";
-import tshirt from "./t-shirt.jpg";
 
-export const assets ={
-  backpack,
-  chair,
-  console,
-  gamingchair,
-  gamingmouse,
-  handbag,
-  headphones,
+export const assets = {
+  backpack1,
+  backpack2,
+  backpack3,
+  backpack4,
+  chair1,
+  chair2,
+  chair3,
+  chair4,
+  console1,
+  console2,
+  console3,
+  console4,
+  gamingchair1,
+  gamingchair2,
+  gamingchair3,
+  gamingchair4,
+  gamingmouse1,
+  gamingmouse2,
+  gamingmouse3,
+  gamingmouse4,
+  handbag1,
+  handbag2,
+  handbag3,
+  handbag4,
+  headphones1,
+  headphones2,
+  headphones3,
+  headphones4,
+  hoodie1,
+  hoodie2,
+  hoodie3,
+  hoodie4,
+  lamp1,
+  lamp2,
+  lamp3,
+  lamp4,
+  laptop1,
+  laptop2,
+  laptop3,
+  laptop4,
+  mechanicalkeyboard1,
+  mechanicalkeyboard2,
+  mechanicalkeyboard3,
+  mechanicalkeyboard4,
+  mobile1,
+  mobile2,
+  mobile3,
+  mobile4,
+  shoes1,
+  shoes2,
+  shoes3,
+  shoes4,
+  smartwatch1,
+  smartwatch2,
+  smartwatch3,
+  smartwatch4,
+  table1,
+  table2,
+  table3,
+  table4,
+  tshirt1,
+  tshirt2,
+  tshirt3,
+  tshirt4,
   heroimage1,
   heroimage2,
   heroimage3,
   heroimage4,
   heroimage5,
   heroimage6,
-  hoodie,
-  lamp,
-  laptop,
-  logo,
-  mechanicalkeyboard,
-  mobile,
-  smartwatch,
-  sneakers,
-  table,
-  tshirt,
   app,
-  play
+  play,
+  logo,
 };
